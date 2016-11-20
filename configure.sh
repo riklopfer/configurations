12184540ed2328466cef3473bb74f689ed3c95ef
rklopfer <russell@klopfer.us>
2016-11-20 02:33:36 -0500

    improved configure script

--- configure.sh
+++ configure.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 LOC=$(dirname $(readlink -f $0))
+BASE_DIR=$LOC/$(uname)
+
+if ! [ -d $BASE_DIR ]; then
+    echo "Unknown OS: $(uname)"
+    exit
+fi
 
 ## 
 # Install software
@@ -13,13 +19,15 @@ LOC=$(dirname $(readlink -f $0))
 # Link configs
 ##
 
-# .bashrc
-# TODO
+# back up .bashrc
+[ -e $HOME/.bashrc ] && mv $HOME/.bashrc $HOME/.bashrc.bak
 
 # .config/ dir
-for TARGET in $(find $LOC/.config -type f); do
-    NAME=$(echo $TARGET | perl -pe "s!$LOC!$HOME!")
+for TARGET in $(find $BASE_DIR -type f); do
+    NAME=$(echo $TARGET | perl -pe "s!$BASE_DIR!$HOME!")
     NAME_DIR=$(dirname $NAME)
     [ -d $NAME_DIR ] || mkdir -p $NAME_DIR
-    ln -s $TARGET $NAME
+
+    echo "ln -fs $TARGET $NAME"
+    ln -fs $TARGET $NAME
 done

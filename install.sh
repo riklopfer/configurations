2eb2203d43053f7691abac76791b6876e87767e3
rklopfer <russell@klopfer.us>
2016-11-20 16:28:17 -0500

    ucommenting all of install

--- install.sh
+++ install.sh
@@ -6,19 +6,19 @@
 if command -v apt >/dev/null 2>&1; then
     echo "Installing software"
     # common 
-    # apt install  chromium-browser
+    sudo apt install i3 chromium-browser
 
     # not sure this will work
     # apt install sublime-text
+    wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_amd64.deb -O sublime-text.deb
+    sudo dpkg -i sublime-text.deb
 
     # terminator
     # apt install i3 terminator python-keybinder
 
     # urxvt
-    # apt install urxvt
-
-    # set urxvt as default terminal
-    # sudo update-alternatives --set x-terminal-emulator $(which urxvt)
+    sudo apt install urxvt
+    sudo update-alternatives --set x-terminal-emulator $(which urxvt)
 
 else
     echo "Not installing software"

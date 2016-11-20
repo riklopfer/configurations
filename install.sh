899980f3d834ea3181f7b964f75015f42646330d
rklopfer <russell@klopfer.us>
2016-11-20 18:12:35 -0500

    also install pip

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@
 if command -v apt >/dev/null 2>&1; then
     echo "Installing software"
     sudo apt update
-    
+
     # window manager and browser
     sudo apt install i3 chromium || exit
 
     # install python
-    sudo apt install python || exit
+    sudo apt install python python-pip || exit
 
     # sublime text
     if ! [ -f /tmp/sublime-text.deb ]; then

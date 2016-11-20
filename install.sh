0910b8d9d5d7c031bab6f1d5e70c302843af5648
rklopfer <russell@klopfer.us>
2016-11-20 16:30:15 -0500

    adding or exit

--- install.sh
+++ install.sh
@@ -6,19 +6,19 @@
 if command -v apt >/dev/null 2>&1; then
     echo "Installing software"
     # common 
-    sudo apt install i3 chromium
+    sudo apt install i3 chromium || exit
 
     # not sure this will work
     # apt install sublime-text
     wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_amd64.deb -O sublime-text.deb
-    sudo dpkg -i sublime-text.deb
+    sudo dpkg -i sublime-text.deb || exit
 
     # terminator
     # apt install i3 terminator python-keybinder
 
     # urxvt
-    sudo apt install urxvt
-    sudo update-alternatives --set x-terminal-emulator $(which urxvt)
+    sudo apt install urxvt || exit
+    sudo update-alternatives --set x-terminal-emulator $(which urxvt) || exit
 
 else
     echo "Not installing software"

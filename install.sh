b8e68f00d1405302f01e61202c8b8cee9b7430de
rklopfer <russell@klopfer.us>
2016-11-20 14:34:36 -0500

    clean up

--- install.sh
+++ install.sh
@@ -5,10 +5,21 @@
 
 if command -v apt >/dev/null 2>&1; then
     echo "Installing software"
-# apt install i3 terminator python-keybinder chromium-browser
+    # common 
+    # apt install  chromium-browser
 
-# not sure this will work
-# apt install sublime-text
+    # not sure this will work
+    # apt install sublime-text
+
+    # terminator
+    # apt install i3 terminator python-keybinder
+
+    # urxvt
+    # apt install urxvt
+
+    # set urxvt as default terminal
+    # sudo update-alternatives --set x-terminal-emulator $(which urxvt)
+
 else
     echo "Not installing software"
 fi

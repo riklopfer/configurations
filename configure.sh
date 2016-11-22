5c05497460410a796b8b72f26629967c6e84cbdc
rklopfer <russell@klopfer.us>
2016-11-22 00:25:41 -0500

    fixing typo

--- configure.sh
+++ configure.sh
@@ -43,7 +43,7 @@ EOF
 ) > $HOME/.pastebinit.xml
 
 # create link to .pushover if it exists where we expect it to be
-[ -f $HOME/Dropbox/Private/.pushover ] && ! [ -L $HOME/.pushover] && ln -fs $HOME/Dropbox/Private/.pushover $HOME/.pushover
+[ -f $HOME/Dropbox/Private/.pushover ] && ! [ -L $HOME/.pushover ] && ln -fs $HOME/Dropbox/Private/.pushover $HOME/.pushover
 
 # apply Xresources changes
 xrdb -merge $HOME/.Xresources

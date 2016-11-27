31f04549a57222a0186091fbf77fe56fcacf4392
rklopfer <russell@klopfer.us>
2016-11-26 23:21:45 -0500

    update after backports

--- debian/pre_install.sh
+++ debian/pre_install.sh
@@ -1,9 +1,10 @@
 #/usr/bin/env bash
 echo
 echo "Running pre-install script"
-sudo apt update
 
 source /etc/os-release
 case $VERSION_ID in
     8) echo "deb http://ftp.debian.org/debian jessie-backports main" | sudo tee -a /etc/sources.list.d/backports.list
 esac
+
+sudo apt update

190374fd053e5100dc85bd6a3fc6d11c715e4866
rklopfer <russell@klopfer.us>
2016-11-26 23:20:30 -0500

    trying to install jessie backports

--- debian/pre_install.sh
+++ debian/pre_install.sh
@@ -2,3 +2,8 @@
 echo
 echo "Running pre-install script"
 sudo apt update
+
+source /etc/os-release
+case $VERSION_ID in
+    8) echo "deb http://ftp.debian.org/debian jessie-backports main" | sudo tee -a /etc/sources.list.d/backports.list
+esac

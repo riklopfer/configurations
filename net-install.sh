7572683cdcda11d7c911d1789c7954bfeafd2998
Russell Klopfer <russell@klopfer.us>
2017-11-06 12:44:46 -0500

    check if repo is already cloned

--- net-install.sh
+++ net-install.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y git 
-mkdir -p ${HOME}/Git/ 
-git clone https://github.com/riklopfer/rig.git ${HOME}/Git/rig
+
+[ -d ${HOME}/Git ] || mkdir -p ${HOME}/Git/ 
+if [ -d ${HOME}/Git/rig ]; then
+    git pull ${HOME}/Git/rig
+else
+    git clone https://github.com/riklopfer/rig.git ${HOME}/Git/rig
+fi
+
 ${HOME}/Git/rig/install.sh 2>&1 | tee log.rig-install
 ${HOME}/Git/rig/configure.sh 2>&1 | tee log.rig-configure

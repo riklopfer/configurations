2a2eb2eaa883a7c7fd5161136cca13cea41fc54d
Russell Klopfer <russell@klopfer.us>
2017-11-06 12:34:31 -0500

    dash y

--- net-install.sh
+++ net-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-sudo apt update && sudo apt upgrade 
-sudo apt install git 
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y git 
 mkdir -p ${HOME}/Git/ 
 git clone https://github.com/riklopfer/rig.git ${HOME}/Git/rig
 ${HOME}/Git/rig/install.sh 

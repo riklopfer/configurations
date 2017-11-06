231706aece70c16cca5c062cd1e25dbade478797
Russell Klopfer <russell@klopfer.us>
2017-11-06 12:42:48 -0500

    logging

--- net-install.sh
+++ net-install.sh
@@ -3,5 +3,5 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y git 
 mkdir -p ${HOME}/Git/ 
 git clone https://github.com/riklopfer/rig.git ${HOME}/Git/rig
-${HOME}/Git/rig/install.sh 
-${HOME}/Git/rig/configure.sh
+${HOME}/Git/rig/install.sh 2>&1 | tee log.rig-install
+${HOME}/Git/rig/configure.sh 2>&1 | tee log.rig-configure
